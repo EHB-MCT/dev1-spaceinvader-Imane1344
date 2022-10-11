291ddedd3fbbcdef0120d150aee96efc033192ea
imane <imane.laalou@student.ehb.be>
2022-10-11 23:16:24 +0200

    excercise space invador

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,47 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+square();
+DrawSpaceInvader();
+
+function square() {
+
+    context.beginPath();
+    context.rect(25, 0, 300, 300);
+    context.fillStyle = "black"
+    context.fill();
+    context.stroke();
+
+}
+
+function DrawSpaceInvader() {
+
+    context.beginPath();
+    context.rect(50, 50, 50, 50);
+    context.rect(50, 100, 50, 50);
+    context.rect(100, 100, 50, 50);
+    context.rect(150, 100, 50, 50);
+    context.rect(150, 50, 50, 50);
+    context.rect(200, 100, 50, 50);
+    context.rect(250, 100, 50, 50);
+    context.rect(250, 50, 50, 50);
+    context.rect(150, 150, 50, 50);
+    context.rect(100, 150, 50, 50);
+    context.rect(200, 150, 50, 50);
+    context.rect(100, 200, 50, 50);
+    context.rect(50, 200, 50, 50);
+    context.rect(200, 200, 50, 50);
+    context.rect(250, 200, 50, 50);
+
+
+
+    context.strokeStyle = "#66FF00"
+    context.fillStyle = "#66FF00"
+    context.fill();
+    context.stroke();
+
+}
